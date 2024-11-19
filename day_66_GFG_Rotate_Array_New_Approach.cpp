7f11a7227107ea9ecb860be7cde5b6b4e61f1612
shivakant kurmi <154341955+shivakantkurmi@users.noreply.github.com>
2024-11-19 12:00:44 +0530

    Create day_66_GFG_Rotate_Array_New_Approach.cpp

---
+++ day_66_GFG_Rotate_Array_New_Approach.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    void rotateArr(vector<int>& arr, int d) {
+        d%=arr.size();
+        reverse(arr.begin() ,  arr.begin()+(d));
+        reverse(arr.begin()+d , arr.end());
+        reverse(arr.begin(),arr.end());
+    
+    }
+};
